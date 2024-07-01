862e01b55865988d32092a5f7cd41f69060e7670
alvaro <alvaroplazalan@gmail.com>
2024-07-01 17:14:34 +0200

    cleaning import lists after refactoring

--- src/facturator/entrypoints/flask_app.py
+++ src/facturator/entrypoints/flask_app.py
@@ -1,17 +1,11 @@
-from datetime import datetime, timedelta
-from pathlib import Path
-import uuid
-
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-import yaml
-from flask_swagger_ui import get_swaggerui_blueprint
 
 from facturator.adapters.database import get_sqlalchemy_session
 from facturator.service_layer.unit_of_work import SqlAlchemyUnitOfWork
 from facturator.entrypoints.resources.api import create_api_blueprint
 from facturator.entrypoints.resources.auth_routes import auth_bp
-from facturator.entrypoints.resources.swagger import create_swagger_blueprint, SWAGGER_URL
+from facturator.entrypoints.resources.swagger import create_swagger_blueprint
 
 
 app = Flask(__name__, template_folder='templates')
@@ -23,24 +17,7 @@ app.register_blueprint(api_bp, url_prefix='/api')
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
 # Swagger UI configuration
-api_spec = yaml.safe_load((Path(__file__).parent / "api_spec.yaml").read_text())
 swagger_bp = create_swagger_blueprint()
-
-# @app.route('/swagger.json')
-# def swagger():
-#     return jsonify(api_spec)
-#
-#
-# SWAGGER_URL = '/apidocs'
-# API_URL = '/swagger.json'
-#
-# swaggerui_blueprint = get_swaggerui_blueprint(
-#     SWAGGER_URL,
-#     API_URL,
-#     config={
-#         'app_name': "Your Flask App"
-#     }
-# )
 app.register_blueprint(swagger_bp)
 
 
